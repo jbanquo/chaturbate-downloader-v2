12ab6de3d4b9995c95526e57e505424417953eb8
jbanquo <jbanquo@users.noreply.github.com>
2016-02-22 17:13:44 +0000

    Initial commit.

---
+++ cb-downloader-v2/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace cb_downloader_v2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.modelsGroupBox = new System.Windows.Forms.GroupBox();
+            this.modelNameTextBox = new System.Windows.Forms.TextBox();
+            this.addModelButton = new System.Windows.Forms.Button();
+            this.modelsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(387, 259);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // modelsGroupBox
+            // 
+            this.modelsGroupBox.Controls.Add(this.checkedListBox1);
+            this.modelsGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.modelsGroupBox.Name = "modelsGroupBox";
+            this.modelsGroupBox.Size = new System.Drawing.Size(399, 292);
+            this.modelsGroupBox.TabIndex = 1;
+            this.modelsGroupBox.TabStop = false;
+            this.modelsGroupBox.Text = "Models";
+            // 
+            // modelNameTextBox
+            // 
+            this.modelNameTextBox.Location = new System.Drawing.Point(12, 310);
+            this.modelNameTextBox.Name = "modelNameTextBox";
+            this.modelNameTextBox.Size = new System.Drawing.Size(318, 20);
+            this.modelNameTextBox.TabIndex = 2;
+            // 
+            // addModelButton
+            // 
+            this.addModelButton.Location = new System.Drawing.Point(336, 310);
+            this.addModelButton.Name = "addModelButton";
+            this.addModelButton.Size = new System.Drawing.Size(75, 23);
+            this.addModelButton.TabIndex = 2;
+            this.addModelButton.Text = "Add";
+            this.addModelButton.UseVisualStyleBackColor = true;
+            this.addModelButton.Click += new System.EventHandler(this.addModelButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 344);
+            this.Controls.Add(this.modelNameTextBox);
+            this.Controls.Add(this.addModelButton);
+            this.Controls.Add(this.modelsGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MainForm";
+            this.Text = "cb-downloader-2";
+            this.modelsGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.GroupBox modelsGroupBox;
+        private System.Windows.Forms.TextBox modelNameTextBox;
+        private System.Windows.Forms.Button addModelButton;
+    }
+}
+
---
+++ cb-downloader-v2/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace cb_downloader_v2
+{
+    public partial class MainForm : Form
+    {
+        private const string ModelsFileName = "models.txt";
+        private const string OutputFolderName = "Recordings";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeListener();
+            LoadModelsFile();
+            PrepareOutputFolder();
+        }
+
+        private async void LoadModelsFile()
+        {
+            // Checking if file exists
+            if (!File.Exists(ModelsFileName))
+                return;
+
+            // Reading model file content
+            StreamReader r = new StreamReader(ModelsFileName);
+            string models = await r.ReadToEndAsync();
+
+            // Parsing lines, we filter out comments and empty lines
+            foreach (string modelName in Regex.Split(models, "\r\n").Where(modelName => modelName.Length != 0 && !modelName.StartsWith("#")))
+            {
+                AddUser(modelName);
+            }
+        }
+
+        private void AddUser(string modelName)
+        {
+            // Debug output
+#if DEBUG
+            Debug.WriteLine("AddUser: " + modelName);
+#endif
+        }
+
+        private void InitializeListener()
+        {
+            // TODO impl
+        }
+
+        private void PrepareOutputFolder()
+        {
+            // Creating recordings folder if non-existant
+            try
+            {
+                if (!Directory.Exists(OutputFolderName))
+                {
+                    Directory.CreateDirectory(OutputFolderName);
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                if (MessageBox.Show(this, "Error creating output folder: " + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void addModelButton_Click(object sender, EventArgs e)
+        {
+            // Checking if input is valid
+            if (string.IsNullOrEmpty(modelNameTextBox.Text))
+                return;
+
+            // Adding user to listener
+            AddUser(modelNameTextBox.Text);
+            modelNameTextBox.Text = "";
+        }
+    }
+}
